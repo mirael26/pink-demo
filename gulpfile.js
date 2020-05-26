82d2f4526ea03c9cdd4038da9a9e23d88d31e2e5
Эльмира <capricorn_26@mail.ru>
2020-05-26 23:03:01 +0700

    Переносит html файлы в корень проекта

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task("css", function () {
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "./",
     notify: false,
     open: true,
     cors: true,
